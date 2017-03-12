c25279bd68e66b16afa6de97ccf6a7e9a276be9c
James <kbj2060@naver.com>
2017-03-12 20:27:53 +0900

    .

--- RL/pythonScript/example04.py
+++ RL/pythonScript/example04.py
@@ -5,6 +5,9 @@ import random as pr
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+def one_hot(x):
+    return np.identity(16)[x: x+1]
+
 register(
     id = 'FrozenLake-v3',
     entry_point = 'gym.envs.toy_text:FrozenLakeEnv',
@@ -19,11 +22,11 @@ input_size = env.observation_space.n
 output_size = env.action_space.n
 lr = 0.1
 
-X = tf.placeholder([1,input_size], dtype = tf.float32)
+X = tf.placeholder(tf.float32, shape = [1, input_size])
 W = tf.Variable(tf.random_uniform([input_size,output_size],0,0.01))
 
 Qpred = tf.matmul(X,W)
-Y = tf.placeholder([1, output_size], tf.float32)
+Y = tf.placeholder( tf.float32, shape = [1, output_size])
 
 loss = tf.reduce_sum(tf.square(Y - Qpred))
 train = tf.train.GradientDescentOptimizer(lr ).minimize(loss)
@@ -42,7 +45,7 @@ with tf.Session() as sess:
         local_loss = []
 
         while not done:
-            Qs = sess.run(Qpred, feed_dict={X : tf.one_hot(s)})
+            Qs = sess.run(Qpred, feed_dict={X : one_hot(s)})
             if np.random.rand(1) < e:
                 a = env.action_space.sample()
             else:
@@ -53,10 +56,13 @@ with tf.Session() as sess:
             if done:
                 Qs[0,a] = reward
             else:
-                Qs1 = sess.run(Qpred, feed_dict={X : tf.one_hot(s1)})
+                Qs1 = sess.run(Qpred, feed_dict={X : one_hot(s1)})
                 Qs[0,a] = reward + dis * np.max(Qs1)
 
-            sess.run(train, feed_dict={X : tf.one_hot(s), Y : Qs})
+            sess.run(train, feed_dict={X : one_hot(s), Y : Qs})
             rAll += reward
             s = s1
         rList.append(rAll)
+        print("Percentage of successful episodes : " + str(sum(rList)/num_ep)+ "%")
+        plt.bar(range(len(rList)), rList, color="blue")
+        plt.show()
